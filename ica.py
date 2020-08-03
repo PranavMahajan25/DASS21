b1c4967e5798c182315feb2b81c9c04ae3a638eb
Raghav Prasad <raghavprasad13@gmail.com>
2020-08-03 10:38:02 +0530

    Modify ICA script to include GUI for selecting subject folder

--- ica.py
+++ ica.py
@@ -1,29 +1,74 @@
-
 # Importing requisite packages
-from os.path import join
+import os
+from os.path import join, exists
+from glob import glob
+from pathlib import Path
+
 import mne
 from mne.preprocessing import ICA
 
-# Loading a preprocessed file and plotting
-data_folder = join('..', 'Data', 'DASS21-Leah-10-01-2018_20180110_032644_fil')
-data_raw_file = join(data_folder, 'Q011_Q02B_raw.fif')
+import tkinter as tk
+from tkinter import filedialog
 
-raw = mne.io.read_raw_fif(data_raw_file, preload=True)
-raw.plot(title='Raw plot', block=True)
+root = tk.Tk()
+root.withdraw()
 
-ica = ICA(n_components=32, random_state=97)
-ica.fit(raw)
 
-# Plotting ICA components
-ica.plot_sources(raw, title='ICA components', block=True)
-'''
-After the ICA components have been plotted, one only needs to
-select the components that are to be excluded from the process
-of signal reconstruction
-'''
+def perform_ica(data_folder, subject):
+    data_raw_files = glob(join(data_folder, '*'))
 
-reconst_raw = raw.copy()    # Creating a copy of the initial raw file
-ica.apply(reconst_raw)      # Applying ICA to the copy
+    for data_raw_file in data_raw_files:
+        segment_name = data_raw_file.split('/')[-1]
 
-# Plotting reconstructed signal
-reconst_raw.plot(title='Reconstructed signal using ICA components', block=True)
+        raw = mne.io.read_raw_fif(data_raw_file, preload=True)
+        raw.plot(title='Raw plot ('+segment_name+')', block=True)
+
+        ica = ICA(n_components=32, random_state=97)
+        ica.fit(raw)
+
+        # Plotting ICA components
+        ica.plot_sources(raw,
+                         title='ICA components ('+segment_name+')',
+                         block=True)
+        '''
+        After the ICA components have been plotted, one only needs to
+        select the components that are to be excluded from the process
+        of signal reconstruction
+        '''
+
+        reconst_raw = raw.copy()    # Creating a copy of the initial raw file
+        ica.apply(reconst_raw)      # Applying ICA to the copy
+
+        # Plotting reconstructed signal
+        reconst_raw.plot(title='Reconstructed signal ('+segment_name+')',
+                         block=True)
+
+        reconst_raw.save(join(reconstructed,
+                              subject,
+                              segment_name), overwrite=True)
+
+
+home = str(Path.home())
+reconstructed = join(home, 'Reconstructed')
+if not exists(reconstructed):
+    os.mkdir(reconstructed)
+
+subjects_chosen = []
+
+while(True):
+    print('Choose a subject folder')
+    data_folder = filedialog.askdirectory()
+    subject = data_folder.split('/')[-1]
+    if subject in subjects_chosen:
+        print('Subject has already been chosen before. Choose another subject')
+        continue
+
+    if not exists(join(reconstructed, subject)):
+        os.mkdir(join(reconstructed, subject))
+
+    perform_ica(data_folder, subject)
+    subjects_chosen.append(subject)
+
+    another_one = input('Do you wish to choose another subject [y/n]: ')
+    if another_one == 'n':
+        break
